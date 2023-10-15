d750876042cecde07b99c225128d01c81e9ee39f
Rana Vivek <ranavik0000005@gmail.com>
2023-10-15 18:29:17 +0530

    Upload project

---
+++ DaynamicTimeTable/Controllers/HomeController.cs
@@ -0,0 +1,74 @@
+﻿using DaynamicTimeTable.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DaynamicTimeTable.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(TimeTable tb)
+        {
+            int total_working = tb.Subject * tb.Days;
+            tb.working_hour = total_working;
+            return View("subjectname",tb);
+        }
+        [HttpPost]
+        public ActionResult submitsubject(TimeTable tb)
+        {
+            int subjectcount = 0, subjectlist = 0;
+            
+            DataTable dt = new DataTable();
+            for(int c = 1;c<=tb.Days;c++)
+            {
+                dt.Columns.Add("Day" + (c));
+            }
+            for(int r = 0; r < tb.Subject; r++)
+            {
+                dt.Rows.Add();
+            }
+
+
+            for(int r = 0;r<dt.Rows.Count;r++)
+            {
+                for(int c = 0;c < dt.Columns.Count;)
+                {
+                 
+                    if(subjectlist < tb.subjectlist.Count)
+                    {
+                        int a = int.Parse(tb.subjectlist[subjectlist].Value);
+                            if ((int.Parse(tb.subjectlist[subjectlist].Value)) > 0)
+                            {
+                                dt.Rows[r][c] = tb.subjectlist[subjectlist].Text;
+                            tb.subjectlist[subjectlist].Value = (int.Parse(tb.subjectlist[subjectlist].Value) - 1).ToString();
+                                c++;
+                            }
+                            else
+                            {
+                                subjectlist++;
+                            }
+                        
+                        
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+                }
+
+            }
+            Session["timetable"] = dt;
+            return View("genratetimetable");
+        }
+     
+    }
+}
---
+++ DaynamicTimeTable/Models/TimeTable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DaynamicTimeTable.Models
+{
+    public class TimeTable
+    {
+       
+        public int Days { get; set; }
+        public int Subject { get; set; }
+        public int working_hour { get; set; }
+
+        public List<SelectListItem> subjectlist { get; set; }
+        
+       
+    }
+}
